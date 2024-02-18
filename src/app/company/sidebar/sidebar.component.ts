654e9b5167648820258a764d8623be1ae072998b
victordatadebug <victorsilva@datadebug.com>
2024-02-18 20:17:49 +0000

    update sidebar

--- src/app/company/sidebar/sidebar.component.ts
+++ src/app/company/sidebar/sidebar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { EventService } from '../../services/event.service';
+import { RouterModule } from '@angular/router';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgbTooltipModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  constructor(private eventService: EventService) { }
+
+  ngOnInit(): void {
+    this.eventService.broadcast('hide-header', true);
+  }
+
+  ngOnDestroy(): void {
+    this.eventService.broadcast('hide-header', false);
+  }
 
 }
